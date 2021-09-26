81b2e636a70c3e650c9db19de36fd1728fdd19c6
yusuke1998 <jhonnyjose1998@gmail.com>
2021-09-26 19:32:28 -0400

    [UPD] agregando screenshot

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TOKEN;
@@ -16,34 +16,10 @@ const plant_types = [
   'parasite',
   'water',
   'wind'
-]
+];
 
 const probability_text = (text) =>  {
   let translate = [
-      { 
-        name: 'Plant type', 
-        value: 'Tipo de Planta' 
-      },
-      { 
-        name: 'Negative event probability', 
-        value: '\n - Probabilidad negativa del evento' 
-      },
-      { 
-        name: 'Positive event probability', 
-        value: '\n - Probabilidad positiva del evento' 
-      },
-      { 
-        name: 'Neutral event probability', 
-        value: '\n - Probabilidad de evento neutral' 
-      },
-      { 
-        name: 'Positive max impact', 
-        value: '\n - Impacto máximo positivo' 
-      },
-      { 
-        name: 'Negative max impact', 
-        value: '\n - Impacto máximo negativo' 
-      },
       {
         name: 'use', 
         value: `\n\n*Se recomienda usar invernadero para mañana.`
@@ -55,13 +31,13 @@ const probability_text = (text) =>  {
   ]
 
   return translate.filter(t => t.name == text)[0].value
-}
+};
 
 bot.onText(/\/clima/, async data => {
-  const { chat, text } = data
-  let msj = 'Debes proporcionar un tipo de planta.'
-  let type = text.split(' ')
-  type = type[1]
+  const { chat, text } = data;
+  let msj = 'Debes proporcionar un tipo de planta.';
+  let type = text.split(' ')[1];
+
   if (type) {
     if (plant_types.includes(type.toLocaleLowerCase())) {
       msj = '';
@@ -70,8 +46,7 @@ bot.onText(/\/clima/, async data => {
         header: '.table-impact-header',
         content: '.table-impact-content',
         pathGreenHouse: '[src="assets/images/useGreenhouse.png"]'
-      }
-      //const browser = await puppeteer.launch();
+      };
       const browser = await puppeteer.launch({
         headless: true,
         ignoreDefaultArgs: ['--disable-extensions'],
@@ -85,22 +60,23 @@ bot.onText(/\/clima/, async data => {
             '--single-process',
         ]
       });
-      // const browser = await puppeteer.launch({
-      //   executablePath: '/usr/bin/chromium-browser'
-      // })
       const page = await browser.newPage();
       
-      await page.setDefaultNavigationTimeout(150000)
+      await page.setDefaultNavigationTimeout(150000);
       await page.goto(url_tools);
+      await page.click('[src="assets/images/es2.svg"]');
+      await page.waitForFunction(
+        'document.querySelector("body").innerText.includes("Predicción para mañana")'
+      );
       await page.waitForSelector('[name="searchTerm"]');
       await page.type('[name="searchTerm"]', type);
 
       const data = await page.evaluate((params) => {
-        const range = n => Array.from(Array(n).keys())
+        const range = n => Array.from(Array(n).keys());
         const { type, pathGreenHouse, header, content } = params
-        const titles = document.querySelectorAll(header)
-        const values = document.querySelectorAll(content)
-        const useGreenhouse = document.querySelector(pathGreenHouse)? true: false
+        const titles = document.querySelectorAll(header);
+        const values = document.querySelectorAll(content);
+        const useGreenhouse = document.querySelector(pathGreenHouse)? true: false;
         return {
             useGreenhouse,
             probability: range(titles.length).map(index => {
@@ -108,7 +84,7 @@ bot.onText(/\/clima/, async data => {
                     name: titles[index].getInnerHTML(),
                     value: values[index].getInnerHTML()
                 }
-            }).filter(a=>a.name != 'Recommended action')
+            }).filter(a=>a.name != 'Acción recomendada')
         };
       }, params);
 
@@ -116,9 +92,13 @@ bot.onText(/\/clima/, async data => {
       
       data.probability.forEach(element => {
         const { name, value } = element
-        msj += `${probability_text(name)}: ${value}.`
+        if (name == 'Tipo de planta'){
+          msj = `Planta de ${value}`
+        }else{
+          msj += `\n - ${name}: ${value}.`;
+        }
       });
-      msj += probability_text(data.useGreenhouse? 'use': 'not use')
+      msj += probability_text(data.useGreenhouse? 'use': 'not use');
     }else{
       msj = 'Debes proporcionar un tipo de planta valido.';
     }  
@@ -126,9 +106,54 @@ bot.onText(/\/clima/, async data => {
   bot.sendMessage(chat.id, msj);
 });
 
+bot.onText(/\/extratools/, async data => {
+  const { chat } = data;
+  const browser = await puppeteer.launch({
+    headless: true,
+    ignoreDefaultArgs: ['--disable-extensions'],
+    args: [
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-first-run',
+        '--no-sandbox',
+        '--no-zygote',
+        '--single-process',
+    ]
+  });
+  const page = await browser.newPage();
+  
+  await page.setDefaultNavigationTimeout(150000);
+  await page.goto(url_tools);
+  await page.click('[src="assets/images/es2.svg"]');
+  await page.waitForFunction(
+    'document.querySelector("body").innerText.includes("Predicción para mañana")'
+  );
+  const table = await page.$('.table-responsive');
+
+  await table.screenshot({ path: 'screenshot.png' });
+  await browser.close();
+
+  bot.sendPhoto(chat.id, './screenshot.png', { caption: 'Creditos: https://www.pvuextratools.com' });
+});
+
+bot.onText(/\/donar/, async data => {
+  const { chat } = data;
+  const msj = 'Hola, Mi wallet metamask (BSC): \n\n 0xd00d845eA2711afDAD00A5B7E4F4719e641C5871'
+  bot.sendMessage(chat.id, msj);
+})
+
 bot.setMyCommands([
   {
     command: "/clima",
-    description: "Con el comando /clima <tipo de planta>, obtienes la probabilidad para el dia de mañana.",
-  }
+    description: "Ejemplo: /clima fire."
+  },
+  {
+    command: "/extratools",
+    description: "Screenshot de la web pvuextratools"
+  },
+  {
+    command: "/donar",
+    description: "Regalame un cafe. Tlg: @jhonnyprz."
+  },
 ]);
